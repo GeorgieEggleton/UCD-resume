ecd7cac67585fc75ab0d4896d8b89a54e3356ef0
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-07-17 13:11:37 +0000

    another commit

--- map.js
+++ map.js
@@ -1,27 +1,28 @@
-// Initialize and add the map
-let map;
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 40.785091,
+            lng: -73.968285
+        }
+    });
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: 50.344, lng: -1.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 3,
-    center: position,
-    mapId: "Map",
-  });
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "here",
-  });
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 
 initMap();
